2d43e79bbb25806b7ba80a3e99da0dd7b12924af
y2onsoo <abbcaog000@naver.com>
2023-04-26 07:16:23 +0900

    서브페이지작업중

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function(){
+  $(".textbox1").hover(function(){
+    $(".img1-f").css("visibility","hidden");
+    $(".img1-b").css("visibility","visible");
+  },function(){
+    $(".img1-f").css("visibility","visible");
+    $(".img1-b").css("visibility","hidden");
+  });
+
+  $(".textbox2").hover(function(){
+    $(".img2-f").css("visibility","hidden");
+    $(".img2-b").css("visibility","visible");
+  },function(){
+    $(".img2-f").css("visibility","visible");
+    $(".img2-b").css("visibility","hidden");
+  });
+
+  $(".textbox3").hover(function(){
+    $(".img3-f").css("visibility","hidden");
+    $(".img3-b").css("visibility","visible");
+  },function(){
+    $(".img3-f").css("visibility","visible");
+    $(".img3-b").css("visibility","hidden");
+  });
+
+  $(".textbox4").hover(function(){
+    $(".img4-f").css("visibility","hidden");
+    $(".img4-b").css("visibility","visible");
+  },function(){
+    $(".img4-f").css("visibility","visible");
+    $(".img4-b").css("visibility","hidden");
+  });
+
+  $(".textbox5").hover(function(){
+    $(".img5-f").css("visibility","hidden");
+    $(".img5-b").css("visibility","visible");
+  },function(){
+    $(".img5-f").css("visibility","visible");
+    $(".img5-b").css("visibility","hidden");
+  });
+});
