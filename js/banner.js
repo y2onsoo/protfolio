d5f50d594bc29a28f913a9072d7f963b58a12ac3
y2onsoo <abbcaog000@naver.com>
2023-05-02 12:36:28 +0900

    수정중

--- js/banner.js
+++ js/banner.js
@@ -22,6 +22,21 @@ document.getElementById("big6").onclick = function () {
   document.querySelector(".big-6").style.display = "block";
   document.getElementById("bg").classList.add("box-bg");
 };
+document.getElementById("big7").onclick = function () {
+  document.querySelector(".big-7").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
+document.getElementById("big8").onclick = function () {
+  document.querySelector(".big-8").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
+document.getElementById("big9").onclick = function () {
+  document.querySelector(".big-9").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
+
+
+
 
 function btn() {
   document.querySelector(".big-1").style.display = "none";
@@ -30,6 +45,9 @@ function btn() {
   document.querySelector(".big-4").style.display = "none";
   document.querySelector(".big-5").style.display = "none";
   document.querySelector(".big-6").style.display = "none";
+  document.querySelector(".big-7").style.display = "none";
+  document.querySelector(".big-8").style.display = "none";
+  document.querySelector(".big-9").style.display = "none";
 
   document.getElementById("bg").classList.remove("box-bg");
 }
