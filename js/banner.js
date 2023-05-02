8c60e4d3b3684ca8ecdafeedf7169a33d82d2fdb
y2onsoo <abbcaog000@naver.com>
2023-05-02 12:08:48 +0900

    banner페이지 작업중

--- js/banner.js
+++ js/banner.js
@@ -10,16 +10,26 @@ document.getElementById("big3").onclick = function () {
   document.querySelector(".big-3").style.display = "block";
   document.getElementById("bg").classList.add("box-bg");
 };
-
-
-
+document.getElementById("big4").onclick = function () {
+  document.querySelector(".big-4").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
+document.getElementById("big5").onclick = function () {
+  document.querySelector(".big-5").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
+document.getElementById("big6").onclick = function () {
+  document.querySelector(".big-6").style.display = "block";
+  document.getElementById("bg").classList.add("box-bg");
+};
 
 function btn() {
   document.querySelector(".big-1").style.display = "none";
   document.querySelector(".big-2").style.display = "none";
   document.querySelector(".big-3").style.display = "none";
-
-
+  document.querySelector(".big-4").style.display = "none";
+  document.querySelector(".big-5").style.display = "none";
+  document.querySelector(".big-6").style.display = "none";
 
   document.getElementById("bg").classList.remove("box-bg");
 }
